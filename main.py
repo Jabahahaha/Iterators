0dbfe9b6eb93e5c93ce8994e85c004d6de7e10f9
Jabahahaha <misha.andreev3004@gmail.com>
2023-12-13 07:40:49 +0100

    Word length iterator

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-class ColorIterator:
-    def __init__(self, colors):
-        self.colors = colors
+class WordLengthIterator:
+    def __init__(self, sentence):
+        self.words = sentence.split()  
         self.index = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if not self.colors:  
+        if self.index >= len(self.words):  
             raise StopIteration
 
-        color = self.colors[self.index]
-        self.index = (self.index + 1) % len(self.colors)  
-        return color
+        word_length = len(self.words[self.index])
+        self.index += 1
+        return word_length
 
-colors = ["red", "green", "blue", "yellow"]
+sentence = "Hello, how are you doing today?"
+word_length_iterator = WordLengthIterator(sentence)
 
-color_iterator = ColorIterator(colors)
-
-for _ in range(10):
-    print(next(color_iterator))
+for word_length in word_length_iterator:
+    print(word_length)
