032d9e76b31f12f9c4330c90baf6c9393646adf1
Jabahahaha <misha.andreev3004@gmail.com>
2023-12-13 07:44:12 +0100

    Days of the week iterator

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-class WordLengthIterator:
-    def __init__(self, sentence):
-        self.words = sentence.split()  
-        self.index = 0
+class DaysOfWeekIterator:
+    def __init__(self, start_day):
+        self.days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        self.start_index = self.days_of_week.index(start_day)  
+        self.index = self.start_index
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index >= len(self.words):  
-            raise StopIteration
+        day = self.days_of_week[self.index]
+        self.index = (self.index + 1) % len(self.days_of_week)  
+        return day
 
-        word_length = len(self.words[self.index])
-        self.index += 1
-        return word_length
+start_day = "Wednesday"
+days_of_week_iterator = DaysOfWeekIterator(start_day)
 
-sentence = "Hello, how are you doing today?"
-word_length_iterator = WordLengthIterator(sentence)
-
-for word_length in word_length_iterator:
-    print(word_length)
+for _ in range(10):
+    print(next(days_of_week_iterator))
